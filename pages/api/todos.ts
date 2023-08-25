005028473e7252504bd3282beac9812356cd9298
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2023-08-25 10:42:26 +0200

    Initial commit

---
+++ pages/api/todos.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  // if(req.method === "GET") {
+
+  // }
+
+  // if(req.method === "POST") {
+
+  // }
+
+  res.status(200).send([
+    { id: 1, text: "Handla mat", done: true },
+    { id: 2, text: "Laga mat", done: false },
+  ]);
+}
